1ad341b07f690a906098f2adb20dce80c90958ad
MichaelSv <michaelsvistunoff@gmail.com>
2022-10-05 04:04:23 +0600

    Задача 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter length of array: ");
+int length = int.Parse(Console.ReadLine()!);
+
+int[] newArray = getArray(length);
+int sum = countPositive(newArray);
+Console.WriteLine($"Your array: [{String.Join(", ", newArray)}] where positive elements: {sum}");
+
+int[] getArray (int size) {
+    int[] newArray = new int[size];
+    for (int i=0; i<5; i++) {
+        Console.Write($"Input element-{i}: ");
+        newArray[i] = int.Parse(Console.ReadLine()!);
+    }
+    return newArray;
+}
+
+int countPositive (int[] array) {
+    int count = 0;
+
+    for (int i=0; i<array.Length; i++) {
+        if (array[i]>0) {
+            count++;
+        }
+    }
+    return count;
+}
