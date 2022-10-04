94152d41de74ec56d87da9595db2fb4a55f2ef72
MichaelSv <michaelsvistunoff@gmail.com>
2022-10-05 04:35:30 +0600

    Задача 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Ener point B1: ");
+double b1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Ener point K1: ");
+double k1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Ener point B2: ");
+double b2 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Ener point K2: ");
+double k2 = int.Parse(Console.ReadLine()!);
+
+double x = (b2 - b1)/(k1 - k2);
+double y = k2*x+b2;
+
+Console.WriteLine($"({x}; {y})");
